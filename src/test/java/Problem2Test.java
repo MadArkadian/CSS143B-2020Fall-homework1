1cf8342d7547ed255ce2ef3fced1d8dde97c795a
Peng Du <rick.peng.du@gmail.com>
2020-09-23 10:32:12 -0700

    initial commit

---
+++ src/test/java/Problem2Test.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class Problem2Test {
+
+    // Example test
+    // Okay to change
+    @Test
+    public void testBubbleSort() {
+        assertTrue(false); // Place holder
+        return;
+    }
+
+    /* Add more tests here */
+}
